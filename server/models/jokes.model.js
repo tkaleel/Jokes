c5525fa2c218f5307aae9385ed159574fc510139
theo <91856536+tkaleel@users.noreply.github.com>
2022-01-24 17:36:29 -0800

    add validations

--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
-const JokeSchema = new mongoose.Schema({
-    setup: String,
-    punchline: String
-});
+const JokeSchema = new mongoose.Schema(
+    {
+    setup: {
+        type: String,
+        required: [true, "Setup is required!"],
+        minlength: [2, "Setup must contain at least 2 characters."]
+    },
+    punchline: {
+        type: String,
+        required: [true, "Punchline is required!"],
+        minlength: [2, "Punchline must contain at least 2 characters."]
+    }
+    },
+    { timestamps: true }
+
+    );
 
 const Joke = mongoose.model('Joke', JokeSchema);
 
